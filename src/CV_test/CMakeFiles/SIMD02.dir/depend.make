1f00e61cbf5dbcb40c2eaeaeee1b0bea5c9a547b
weison <weisonweileen@gmail.com>
2024-04-05 15:03:42 +0800

    remove too big pt file

--- src/CV_test/CMakeFiles/SIMD02.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SIMD02.
-# This may be replaced when dependencies are built.
--- src/CV_test/test_yolov8.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-# model = YOLO('hand_detection/best_cups_2024_1_26.pt')
-model = YOLO('/home/ws/src/CV_test/2024_400_pict.pt')
-
-results = model(source=0, show=True, conf = 0.4)
