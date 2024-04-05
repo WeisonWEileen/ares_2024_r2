ff5a9776f67e8813d6d050b3a98dbed89b900d94
weison <weisonweileen@gmail.com>
2024-04-05 21:52:57 +0800

    DEBUG git too big files

--- src/letsgo.sh
+++ src/letsgo.sh
@@ -1,15 +1,31 @@
-ros2 launch rc_serial_driver serial_driver.launch.py
-ros2 launch armor_detector detector.launch.py
-ros2 run v4l2_camera v4l2_camera_node 
-# ros2 run v4l2_camera v4l2_camera_node --ros-args -p video_device:=/dev/cam0
-ros2 run rqt_image_view rqt_image_view
-ros2 launch yolov8_bringup yolov8.launch.py
+# ros2 launch rc_serial_driver serial_driver.launch.py
+# ros2 launch armor_detector detector.launch.py
+# ros2 run v4l2_camera v4l2_camera_node 
+# # ros2 run v4l2_camera v4l2_camera_node --ros-args -p video_device:=/dev/cam0
+# ros2 run rqt_image_view rqt_image_view
+# ros2 launch yolov8_bringup yolov8.launch.py
 
 
 
 
 
-#查看摄像头信息
-#查看长宽是640/480
-v4l2-ctl -d  /dev/video0 --all
+# #查看摄像头信息
+# #查看长宽是640/480
+# v4l2-ctl -d  /dev/video0 --all
 
+
+
+# cd ..
+
+cmds=(
+    "ros2 run v4l2_camera v4l2_camera_node"
+	"ros2 launch yolov8_bringup yolov8.launch.py"
+    "ros2 topic echo /yolo/tracking"
+)
+
+for cmd in "${cmds[@]}"
+do 
+    echo Current CMD : "$cmd"
+    gnome-terminal -- bash -c "cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+    sleep 0.2
+done
