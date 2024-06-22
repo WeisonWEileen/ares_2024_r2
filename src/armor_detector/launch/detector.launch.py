cac1e6a8e060ab287777a8fe1dee92121c1aa3f9
weison <weisonweileen@gmaikl.com>
2024-06-22 15:20:23 +0800

    ADD integrated realsense launch
    Signed-off-by: weison <weisonweileen@gmaikl.com>

--- src/armor_detector/launch/detector.launch.py
+++ src/armor_detector/launch/detector.launch.py
@@ -3,13 +3,24 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
 
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # config = os.path.join(
-    #     get_package_share_directory('armor_detector'), 'config', 'serial_driver.yaml')
-
-    rc_armor_detector_node = Node(
+    realsense_launch = IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource([
+                        PathJoinSubstitution([
+                            FindPackageShare('realsense2_camera'), 'launch', 'rs_launch.py'
+                            ])
+                        ]),
+                    launch_arguments={
+                        'align_depth.enable': 'true'
+                        }.items(),
+                    )
+    rc_detector_node = Node(
         package='armor_detector',
         executable='rc_armor_detector_node',
         namespace='',
@@ -18,7 +29,7 @@ def generate_launch_description():
         # parameters=[config],
     )
 
-    rc_armor_projector_node = Node(
+    rc_projector_node = Node(
         package="armor_detector",
         executable="rc_armor_projector_node",
         namespace="",
@@ -27,4 +38,9 @@ def generate_launch_description():
         # parameters=[config],
     )
 
-    return LaunchDescription([rc_armor_detector_node,rc_armor_projector_node])
+    ld = LaunchDescription()
+    ld.add_action(realsense_launch)
+    ld.add_action(rc_detector_node)
+    ld.add_action(rc_projector_node)
+
+    return ld
